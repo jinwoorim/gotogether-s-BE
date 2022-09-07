91a6868a4e7ee765f8051845e55ffcc2dd717659
khjun723 <67496091+khjun723@users.noreply.github.com>
2022-09-07 20:10:53 +0900

    conf : Create Project And Package 프로젝트 생성 및 패키지 추가

---
+++ src/test/java/com/gotogether/gotogethersbe/GotogetherSBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotogether.gotogethersbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotogetherSBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
