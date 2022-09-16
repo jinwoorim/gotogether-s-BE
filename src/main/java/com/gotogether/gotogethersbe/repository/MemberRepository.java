dae418583087e64656b840fb6a1909a541a38339
iheese <imbc0224@naver.com>
2022-09-16 23:42:17 +0900

    feat : MemberRepository 수정

--- src/main/java/com/gotogether/gotogethersbe/repository/MemberRepository.java
+++ src/main/java/com/gotogether/gotogethersbe/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.gotogether.gotogethersbe.repository;
 
 import com.gotogether.gotogethersbe.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
