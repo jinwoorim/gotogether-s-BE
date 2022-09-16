f33a604977659690fe36ed6e21b48f5f527ae8a7
iheese <imbc0224@naver.com>
2022-09-16 23:41:54 +0900

    feat : Member 엔티티 수정

--- src/main/java/com/gotogether/gotogethersbe/domain/Member.java
+++ src/main/java/com/gotogether/gotogethersbe/domain/Member.java
@@ -1,4 +1,44 @@
 package com.gotogether.gotogethersbe.domain;
 
+import com.gotogether.gotogethersbe.domain.enums.Gender;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //기본키
+
+    @Column(nullable = false)
+    private String email; //이메일
+
+    @Column(nullable = false)
+    private String password; //비밀번호
+
+    @Column(nullable = false)
+    private String name; //이름
+
+    private Date birth; //생일
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender; //성별(MALE, FEMALE)
+
+    private String age; //연령대 그룹
+
+    private String companion; //동행자 유형
+
+    private String religion; //종교적 성향
+
+    private String groupType; //그룹 유형
+
+    private String theme; //여행 테마
+
+    private String season; //여행 시기
+
+    private String interest; //관심있는 여행지
 }
