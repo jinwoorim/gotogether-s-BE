da878511ff08592312f760b69e48133142409155
iheese <imbc0224@naver.com>
2022-09-17 13:39:52 +0900

    feat : Transactional 에러 수정

--- src/main/java/com/gotogether/gotogethersbe/service/AuthService.java
+++ src/main/java/com/gotogether/gotogethersbe/service/AuthService.java
@@ -3,11 +3,10 @@ package com.gotogether.gotogethersbe.service;
 import com.gotogether.gotogethersbe.domain.Member;
 import com.gotogether.gotogethersbe.dto.LoginDto;
 import com.gotogether.gotogethersbe.repository.MemberRepository;
-import com.gotogether.gotogethersbe.web.api.DefaultRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
