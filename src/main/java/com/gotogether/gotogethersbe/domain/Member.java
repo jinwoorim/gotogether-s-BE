fd787fc230b37730dca3b5e5741114374b0c5359
iheese <imbc0224@naver.com>
2022-09-20 00:48:04 +0900

    feat : builder 수정

--- src/main/java/com/gotogether/gotogethersbe/domain/Member.java
+++ src/main/java/com/gotogether/gotogethersbe/domain/Member.java
@@ -1,13 +1,14 @@
 package com.gotogether.gotogethersbe.domain;
 
 import com.gotogether.gotogethersbe.domain.enums.Authority;
-import com.gotogether.gotogethersbe.domain.enums.Gender;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.*;
 import java.sql.Date;
 
+@AllArgsConstructor
 @Data
 @Entity
 public class Member {
@@ -30,27 +31,11 @@ public class Member {
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
-    @Enumerated(EnumType.STRING)
-    private Gender gender; //성별(MALE, FEMALE)
-
-    private String age; //연령대 그룹
-
-    private String companion; //동행자 유형
-
-    private String religion; //종교적 성향
-
-    private String groupType; //그룹 유형
-
-    private String theme; //여행 테마
-
-    private String season; //여행 시기
-
-    private String interest; //관심있는 여행지
-
     @Builder
-    public Member(String email, String password, Authority authority) {
+    public Member(String email, String password, String name, Authority authority) {
         this.email = email;
         this.password = password;
+        this.name = name;
         this.authority = authority;
     }
 }
