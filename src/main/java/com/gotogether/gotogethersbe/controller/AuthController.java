aef9fb31f4cff1d2c5b7e718dc1a2dbafe6b8593
iheese <imbc0224@naver.com>
2022-09-20 00:50:20 +0900

    feat : 회원가입, 토큰 재발행 메소드 추가

--- src/main/java/com/gotogether/gotogethersbe/controller/AuthController.java
+++ src/main/java/com/gotogether/gotogethersbe/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.gotogether.gotogethersbe.controller;
 
 import com.gotogether.gotogethersbe.dto.LoginDto;
+import com.gotogether.gotogethersbe.dto.MemberDto;
+import com.gotogether.gotogethersbe.dto.TokenDto;
 import com.gotogether.gotogethersbe.service.AuthService;
 import com.gotogether.gotogethersbe.web.api.DefaultRes;
 import com.gotogether.gotogethersbe.web.api.ResponseMessage;
@@ -16,9 +18,22 @@ public class AuthController {
 
     private final AuthService authService;
 
+    //회원가입
+    @PostMapping("/signup")
+    public DefaultRes signup(@RequestBody MemberDto.MemberRequest request){
+        authService.signup(request);
+        return new DefaultRes(StatusCode.OK, ResponseMessage.CREATED_USER);
+    }
+
     //로그인
     @PostMapping("/login")
     public DefaultRes login(@RequestBody LoginDto.LoginRequest request){
         return new DefaultRes(StatusCode.OK, ResponseMessage.LOGIN_SUCCESS, authService.login(request));
+    }
+
+    //토큰 재발급
+    @PostMapping("/reissue")
+    public DefaultRes reissue(@RequestBody TokenDto tokenRequestDto) {
+        return new DefaultRes(StatusCode.OK, ResponseMessage.REISSUE_SUCCESS,authService.reissue(tokenRequestDto));
     }
 }
