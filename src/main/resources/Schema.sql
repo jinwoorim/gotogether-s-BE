13aac048d433b837d57a295429ca6273343152dc
jinwoorim <akdlshvlth11@naver.com>
2022-09-20 00:58:58 +0900

    refactor: Schema.sql 리팩토링 및 파일 이름 변경

--- src/main/resources/Schema.sql
+++ src/main/resources/Schema.sql
@@ -1,94 +1,76 @@
-    DROP TABLE if EXISTS book CASCADE;
-    DROP TABLE if EXISTS booker CASCADE;
-    DROP TABLE if EXISTS member CASCADE;
-    DROP TABLE if EXISTS product CASCADE;
-    DROP TABLE if EXISTS wish CASCADE;
+    --  workbench에서 DB 이름 : goto 로 생성
+    --  application.yml에 DB 설정 3줄 본인에 맞게 변경
+    --  url: jdbc:mysql://mysql-container:3306/goto?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true
+    --  username: khjun723 < 본인 계정 이름으로 변경
+    --  password: 1324 < 본인 비밀 번호로 변경
 
-    CREATE TABLE book
-    (
-        book_id        BIGINT NOT NULL AUTO_INCREMENT,
-        duration       VARCHAR(255),
-        total_price    long,
-        option_list    VARCHAR(255),
-        product_id     BIGINT,
-        member_id      BIGINT,
-        booker_id      BIGINT,
+    DROP TABLE IF EXISTS Book;
+    DROP TABLE IF EXISTS Booker;
+    DROP TABLE IF EXISTS Curation;
+    DROP TABLE IF EXISTS Member;
+    DROP TABLE IF EXISTS Product;
+    DROP TABLE IF EXISTS Wish;
 
-        PRIMARY KEY (book_id)
+    create table Book(
+                           id             bigint               auto_increment        PRIMARY KEY ,
+                           productId      bigint,
+                           memberId       bigint,
+                           bookerId       bigint,
+                           departureDate  DATE                 not null ,
+                           arrivalDate    DATE                 not null ,
+                           departureArea  varchar(255)         not null ,
+                           selection      varchar(255)         not null ,
+                           totalPrice     long                 not null ,
+                           status         varchar(255)
     );
 
-    CREATE TABLE booker
-    (
-        booker_id    BIGINT NOT NULL AUTO_INCREMENT,
-        name         VARCHAR(255),
-        age          Int,
-        phone_number VARCHAR(255),
-        role         BOOLEAN,
-
-        PRIMARY KEY (booker_id)
+    create table Booker(
+                           id             bigint               auto_increment       PRIMARY KEY ,
+                           name           varchar(255)         not null ,
+                           age            int ,
+                           phoneNumber    varchar(255)         not null ,
+                           role           boolean              not null
     );
 
-    CREATE TABLE member
-    (
-        member_id    BIGINT NOT NULL AUTO_INCREMENT,
-        email        VARCHAR(255),
-        password     VARCHAR(255),
-        name         VARCHAR(255),
-        birth        Date,
-        gender       VARCHAR(255),
-        age          VARCHAR(255),
-
-        PRIMARY KEY (member_id)
+    create table Curation(
+                            id             bigint              auto_increment       PRIMARY KEY ,
+                            ages           varchar(255)        not null ,
+                            together       varchar(255)        not null ,
+                            companion      varchar(255)        not null ,
+                            religion       varchar(255)        not null ,
+                            theme          varchar(255)        not null
     );
 
-    CREATE TABLE product
-    (
-        product_id       BIGINT NOT NULL AUTO_INCREMENT,
-        thumbnail        VARCHAR(255),
-        product_name     VARCHAR(255),
-        amount           Long,
-        ctg_nation       VARCHAR(255),
-        ctg_group        VARCHAR(255),
-        ctg_theme        VARCHAR(255),
-        summary          VARCHAR(255),
-        detail           VARCHAR(255),
-        info             VARCHAR(255),
-        way              VARCHAR(255),
-        info_image       VARCHAR(255),
-
-        PRIMARY KEY (product_id)
+    create table Member(
+                            id             bigint              auto_increment       PRIMARY KEY ,
+                            curationId     bigint                       ,
+                            email          varchar(255)        not null ,
+                            password       varchar(255)        not null ,
+                            name           varchar(255)        not null ,
+                            birth          DATE                not null
     );
 
-    CREATE TABLE wish
-    (
-        wish_id        BIGINT NOT NULL AUTO_INCREMENT,
-        product_id     BIGINT,
-        member_id      BIGINT,
-
-        PRIMARY KEY (wish_id)
+    create table Product(
+                            id             bigint              auto_increment       PRIMARY KEY ,
+                            thumbnail      varchar(255)        not null ,
+                            productName    varchar(255)        not null ,
+                            amount         long                not null ,
+                            country        varchar(255)        not null ,
+                            ages           varchar(255)        not null ,
+                            together       varchar(255)        not null ,
+                            companion      varchar(255)        not null ,
+                            religion       varchar(255)        not null ,
+                            theme          varchar(255)        not null ,
+                            ctgNation      varchar(255)        not null ,
+                            summary        varchar(255)        not null ,
+                            detail         varchar(255)        not null ,
+                            info           varchar(255)        not null ,
+                            way            varchar(255)        not null ,
+                            infoImage      varchar(255)        not null
     );
 
-    alter table book
-        add constraint FKka98u01ogtkot3g9ibmt3qfr4
-        foreign key (product_id)
-        references product;
-
-    alter table book
-        add constraint FKpdmglejuicm0m2wwgvosuv0nq
-        foreign key (member_id)
-        references member;
-
-    alter table book
-        add constraint FKff7t7qd30eapf6och95jhmh1w
-        foreign key (booker_id)
-        references booker;
-
-    alter table wish
-        add constraint FKh3bvkvkslnehbxqma1x2eynqb
-        foreign key (product_id)
-        references product;
-
-    alter table wish
-        add constraint FK70nrc4a6uvljrtemsn80eq1gd
-        foreign key (member_id)
-        references member;
+    create table Wish(
+                            id             bigint              auto_increment       PRIMARY KEY ,
+                            productId      bigint,
+                            memberId       bigint
+    );
