3c0c17f4ab0f85f809704c2cdaaabba2ac591398
iheese <imbc0224@naver.com>
2022-09-20 00:49:19 +0900

    feat : LoginDto 수정

--- src/main/java/com/gotogether/gotogethersbe/dto/LoginDto.java
+++ src/main/java/com/gotogether/gotogethersbe/dto/LoginDto.java
@@ -5,38 +5,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
-import java.sql.Date;
-
 public class LoginDto {
 
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
-    public class LoginRequest {
+    public static class LoginRequest {
         private String email;
         private String password;
 
         public UsernamePasswordAuthenticationToken toAuthentication() {
             return new UsernamePasswordAuthenticationToken(email, password);
         }
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public class LoginResponse {
-        private Long id;
-        private String email;
-        private String name;
-        private Date birth;
-        private String gender;
-        private String age;
-        private String companion;
-        private String religion;
-        private String groupType;
-        private String theme;
-        private String season;
-        private String interest;
-
     }
 }
