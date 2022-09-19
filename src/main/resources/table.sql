54d5004e6b80237e9ded23882dcfab4138736f2e
khjun723 <67496091+khjun723@users.noreply.github.com>
2022-09-19 13:49:23 +0900

    conf : Refactoring gotoDB Table Product table에 country 컬럼 추가, enum값에 상관없음 추가

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -17,12 +17,12 @@ create table Member(
                        name        varchar(100)         not null ,
                        birth       DATE                 not null ,
                        gender      enum('남', '여') ,
-                       groupAge    enum('2030 그룹', '3040 그룹', '4050 그룹','5060 그룹','6070 그룹') ,
-                       companion   enum('나홀로 참가', '친구나 동료', '연인이나 부부', '자녀를 동반하는 가족'),
-                       religion    enum('주님', '부처님'),
-                       groupType   enum('여자끼리 그룹', '남자끼리 그룹', '자녀동반 그룹'),
-                       theme       enum('문화탐방', '골프여행', '리조트 휴양 및 힐링', '오지탐험', '트레킹여행', '봉사활동', '성지순례'),
-                       season      enum('12월~2월', '3월~5월', '6월~8월', '9월~11월'),
+                       groupAge    enum('2030 그룹', '3040 그룹', '4050 그룹','5060 그룹','6070 그룹', '상관없음') ,
+                       companion   enum('나홀로 참가', '친구나 동료', '연인이나 부부', '자녀를 동반하는 가족', '상관없음'),
+                       religion    enum('주님', '부처님', '상관없음'),
+                       groupType   enum('여자끼리 그룹', '남자끼리 그룹', '자녀동반 그룹', '상관없음'),
+                       theme       enum('문화탐방', '골프여행', '리조트 휴양 및 힐링', '오지탐험', '트레킹여행', '봉사활동', '성지순례', '상관없음'),
+                       season      enum('봄', '여름', '가을', '겨울', '상관없음'),
                        trvlDstnIntr varchar(255)
 );
 
@@ -30,6 +30,7 @@ create table Product(
                         id          bigint              auto_increment        PRIMARY KEY ,
                         thumbnail   varchar(255)        not null ,
                         productName varchar(255)        not null ,
+                        country     varchar(255)        not null ,
                         amount      Long                not null ,
                         ctgNation   enum('동남아/태평양', '인도/중앙아시아', '아프리카/중동', '유럽/코카서스', '중남미/북미', '대만/중국/일본') ,
                         ctgGroup    enum('5070끼리', '2040 끼리', '남자끼리' , '여자끼리', '자녀동반'),
@@ -50,17 +51,17 @@ create table Booker(
 );
 
 create table Wish(
-                         id          bigint             auto_increment         PRIMARY KEY ,
-                         productId   bigint,
-                         memberId    bigint
+                     id          bigint             auto_increment         PRIMARY KEY ,
+                     productId   bigint,
+                     memberId    bigint
 );
 
 create table Book(
-                         id          bigint            auto_increment         PRIMARY KEY ,
-                         productId    bigint,
-                         memberId     bigint,
-                         bookerId     bigint,
-                         duration     varchar(100)      not null ,
-                         totalPrice   Long              not null ,
-                         optionList   varchar(255)
+                     id          bigint            auto_increment         PRIMARY KEY ,
+                     productId    bigint,
+                     memberId     bigint,
+                     bookerId     bigint,
+                     duration     varchar(100)      not null ,
+                     totalPrice   Long              not null ,
+                     optionList   varchar(255)
 );
