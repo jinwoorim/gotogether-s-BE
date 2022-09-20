f955a563a259c061195d8eb1a3f522e1d3a80447
jinwoorim <akdlshvlth11@naver.com>
2022-09-20 16:16:37 +0900

    feat: Curation Entity 클래스 생성

---
+++ src/main/java/com/gotogether/gotogethersbe/domain/Curation.java
@@ -0,0 +1,35 @@
+package com.gotogether.gotogethersbe.domain;
+
+import com.gotogether.gotogethersbe.enums.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Curation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CURATION_ID")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Ages ages;
+
+    @Enumerated(EnumType.STRING)
+    private Together together;
+
+    @Enumerated(EnumType.STRING)
+    private Companion companion;
+
+    @Enumerated(EnumType.STRING)
+    private Religion religion;
+
+    @Enumerated(EnumType.STRING)
+    private Theme theme;
+}
