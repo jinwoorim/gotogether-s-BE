76d478f3df5378960b89457f56118fdf14ce1574
iheese <imbc0224@naver.com>
2022-09-21 01:53:49 +0900

    feat : 주석 설명 추가 및 로직 수정

--- src/main/java/com/gotogether/gotogethersbe/web/api/DefaultRes.java
+++ src/main/java/com/gotogether/gotogethersbe/web/api/DefaultRes.java
@@ -1,11 +1,9 @@
 package com.gotogether.gotogethersbe.web.api;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 @Builder
 public class DefaultRes<T> {
 
@@ -13,14 +11,15 @@ public class DefaultRes<T> {
     private String responseMessage;
     private T data;
 
-    public DefaultRes(final int statusCode, final String responseMessage) {
-        this.statusCode = statusCode;
-        this.responseMessage = responseMessage;
-        this.data = null;
-    }
+    /* 메소드에서 메소드를 호출하는 형태에서 직관적으로 변경하였고 굳이 두 가지 방식을
+    사용할 이유를 느끼지 못해서 빌더 생성자만 냅뒀습니다.
+     */
 
     public static<T> DefaultRes<T> res(final int statusCode, final String responseMessage) {
-        return res(statusCode, responseMessage, null);
+        return DefaultRes.<T>builder()
+                .statusCode(statusCode)
+                .responseMessage(responseMessage)
+                .build();
     }
 
     public static<T> DefaultRes<T> res(final int statusCode, final String responseMessage, final T t) {
