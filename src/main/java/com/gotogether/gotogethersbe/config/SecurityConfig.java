ee341306c17e5250012f8638d54a569039d1955a
iheese <imbc0224@naver.com>
2022-09-20 18:02:32 +0900

    conf : 스프링시큐리티 설정 파일 수정

--- src/main/java/com/gotogether/gotogethersbe/config/SecurityConfig.java
+++ src/main/java/com/gotogether/gotogethersbe/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
         http.csrf().disable() //csrf 토큰 막기
                 .authorizeRequests((authz) -> authz
-                        .antMatchers("/login", "/join", "/reissue", "/logout","/products").permitAll()
+                        .antMatchers("/login", "/signup", "/reissue", "/logout","/products").permitAll()
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 ) //아래 요청은 모두 허용, 이외 요청은 인증 필수
                 .authorizeRequests((authz) -> authz
