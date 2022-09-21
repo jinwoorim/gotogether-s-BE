e2e1ed7518a308f86d90061fe51e710be90f4420
iheese <imbc0224@naver.com>
2022-09-21 14:21:19 +0900

    chore : 주석 설명 삭제

--- src/main/java/com/gotogether/gotogethersbe/web/api/DefaultRes.java
+++ src/main/java/com/gotogether/gotogethersbe/web/api/DefaultRes.java
@@ -11,10 +11,6 @@ public class DefaultRes<T> {
     private String responseMessage;
     private T data;
 
-    /* 메소드에서 메소드를 호출하는 형태에서 직관적으로 변경하였고 굳이 두 가지 방식을
-    사용할 이유를 느끼지 못해서 빌더 생성자만 냅뒀습니다.
-     */
-
     public static<T> DefaultRes<T> res(final int statusCode, final String responseMessage) {
         return DefaultRes.<T>builder()
                 .statusCode(statusCode)
