0ddce94a6c6fe5625ac5e144f4a853fe698c033f
iheese <imbc0224@naver.com>
2022-09-22 21:48:06 +0900

    feat : Curation 기능 추가

---
+++ src/main/java/com/gotogether/gotogethersbe/controller/CurationController.java
@@ -0,0 +1,37 @@
+package com.gotogether.gotogethersbe.controller;
+
+import com.gotogether.gotogethersbe.config.util.SecurityUtil;
+import com.gotogether.gotogethersbe.dto.CurationDto;
+import com.gotogether.gotogethersbe.service.CurationService;
+import com.gotogether.gotogethersbe.web.api.DefaultRes;
+import com.gotogether.gotogethersbe.web.api.ResponseMessage;
+import com.gotogether.gotogethersbe.web.api.StatusCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@RestController
+public class CurationController {
+
+    private final CurationService curationService;
+
+    @PostMapping("/members/products")
+    public DefaultRes insertCuration(@RequestBody CurationDto.CurationRequest curationRequest, HttpServletRequest request){
+
+        //로그인 상태면 회원 정보에 큐레이션 데이터 연결
+        Long memberId = SecurityUtil.getCurrentMemberId();
+        if(memberId!=null){
+            curationService.insertCuration(curationRequest, memberId);
+            return DefaultRes.res(StatusCode.OK, ResponseMessage.RECO_DATA_SUCCESS_FOR_MEMBER);
+        }
+        //비로그인 상태면 세션에 큐레이션 데이터 저장
+        HttpSession session = request.getSession();
+        session.setAttribute("curation", curationRequest);
+        return DefaultRes.res(StatusCode.OK, ResponseMessage.RECO_DATA_SUCCESS_FOR_NONMEMBER);
+    }
+}
---
+++ src/main/java/com/gotogether/gotogethersbe/dto/CurationDto.java
@@ -0,0 +1,27 @@
+package com.gotogether.gotogethersbe.dto;
+
+import com.gotogether.gotogethersbe.domain.Curation;
+import com.gotogether.gotogethersbe.domain.enums.*;
+import lombok.Data;
+
+public class CurationDto {
+
+    @Data
+    public static class CurationRequest{
+        private Ages ages;
+        private GenderGroup genderGroup;
+        private Companion companion;
+        private Religion religion;
+        private Theme theme;
+
+        public Curation toCuration(){
+            return Curation.builder()
+                    .ages(ages)
+                    .genderGroup(genderGroup)
+                    .companion(companion)
+                    .religion(religion)
+                    .theme(theme)
+                    .build();
+        }
+    }
+}
