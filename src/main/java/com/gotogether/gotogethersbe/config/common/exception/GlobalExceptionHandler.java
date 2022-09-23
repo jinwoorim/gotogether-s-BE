3154f6026a9ed09c9e657a5046e3aba15935b1a5
iheese <imbc0224@naver.com>
2022-09-23 16:24:59 +0900

    feat : 회원 비밀번호 틀렸을 때 예외 리턴 처리

--- src/main/java/com/gotogether/gotogethersbe/config/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gotogether/gotogethersbe/config/common/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -17,7 +18,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     //커스텀 예외 처리
     @ExceptionHandler(value = { CustomException.class })
     protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
-        log.error("handleCustomException throw CustomException : {}", e.getErrorMessage());
+        log.error("handleCustomException throw CustomException : ", e.getErrorMessage());
         return ErrorResponse.toResponseEntity(e.getErrorMessage(), e.getStatusCode());
     }
 
@@ -26,5 +27,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleDataException() {
         log.error("handleDataException throw Exception : CREATED_FAIL");
         return ErrorResponse.toResponseEntity(ResponseMessage.CREATED_FAIL, StatusCode.BAD_REQUEST);
+    }
+
+    //로그인 예외 처리
+    @ExceptionHandler(value = {BadCredentialsException.class})
+    protected ResponseEntity<ErrorResponse> handleLoginException() {
+        log.error("handleDataException throw Exception : LOGIN_FAIL");
+        return ErrorResponse.toResponseEntity("회원 비밀번호가 틀렸습니다.", StatusCode.UNAUTHORIZED);
     }
 }
