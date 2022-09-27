55b9ee1a5d34d1132d7644961f2bf8001f43e764
iheese <imbc0224@naver.com>
2022-09-27 22:55:00 +0900

    feat : AdminProductDto 기능 구현 및 주석 추가

--- src/main/java/com/gotogether/gotogethersbe/dto/AdminProductDto.java
+++ src/main/java/com/gotogether/gotogethersbe/dto/AdminProductDto.java
@@ -1,9 +1,15 @@
 package com.gotogether.gotogethersbe.dto;
 
+import com.gotogether.gotogethersbe.domain.Option;
+import com.gotogether.gotogethersbe.domain.Product;
 import com.gotogether.gotogethersbe.domain.enums.*;
+import lombok.Getter;
+
+import java.util.List;
 
 public class AdminProductDto {
 
+    @Getter
     public static class ProductRequest{
         private String thumbnail;
         private String productName;
@@ -11,6 +17,7 @@ public class AdminProductDto {
         private String info;
         private String country;
         private Continent continent;
+        private List<OptionDto.OptionRequest> optionDtoList;
 
         //요약 정보
         private String region;
@@ -18,10 +25,30 @@ public class AdminProductDto {
         private String points;
 
         //추천 데이터
-        private Ages ages;
+        private Ages ages; //다수 선택으로 변경 예정
         private Companion companion;
         private GenderGroup genderGroup;
         private Religion religion;
         private Theme theme;
+
+        public Product toProduct(List<Option> optionList){
+            return Product.builder()
+                    .thumbnail(thumbnail)
+                    .productName(productName)
+                    .amount(amount)
+                    .info(info)
+                    .country(country)
+                    .continent(continent)
+                    .optionList(optionList)
+                    .region(region)
+                    .airport(airport)
+                    .points(points)
+                    .ages(ages)
+                    .companion(companion)
+                    .genderGroup(genderGroup)
+                    .religion(religion)
+                    .theme(theme)
+                    .build();
+        }
     }
 }
