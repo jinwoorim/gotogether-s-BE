dfedec60c62b1d298752d8b345901baf861cf045
iheese <imbc0224@naver.com>
2022-09-27 22:52:04 +0900

    feat :  상품 등록, 옵션 등록 기능 구현

---
+++ src/main/java/com/gotogether/gotogethersbe/repository/OptionRepository.java
@@ -0,0 +1,7 @@
+package com.gotogether.gotogethersbe.repository;
+
+import com.gotogether.gotogethersbe.domain.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Long> {
+}
--- src/main/java/com/gotogether/gotogethersbe/service/AdminService.java
+++ src/main/java/com/gotogether/gotogethersbe/service/AdminService.java
@@ -1,13 +1,34 @@
 package com.gotogether.gotogethersbe.service;
 
+import com.gotogether.gotogethersbe.domain.Option;
+import com.gotogether.gotogethersbe.dto.AdminProductDto;
+import com.gotogether.gotogethersbe.repository.OptionRepository;
+import com.gotogether.gotogethersbe.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class AdminService {
 
-    private final AdminService adminService;
+    private final ProductRepository productRepository;
+    private final OptionRepository optionRepository;
 
+    //상품 등록
+    @Transactional
+    public void insertProduct(AdminProductDto.ProductRequest request){
+        List<Option> optionList  = request.getOptionDtoList()
+                                   .stream()
+                                   .map(optionDto -> optionDto.toOption())
+                                   .toList();
+        optionRepository.saveAll(optionList
+                                   .stream()
+                                   .map(option -> option.addProduct(request.toProduct(optionList)))
+                                   .toList());
+        productRepository.save(request.toProduct(optionList));
+    }
 
 }
