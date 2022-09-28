36dd0ee60a7d02ad6585162fd177362ff9f2ecd5
iheese <imbc0224@naver.com>
2022-09-28 20:47:40 +0900

    feat : OptionRepository 추가

--- src/main/java/com/gotogether/gotogethersbe/repository/OptionRepository.java
+++ src/main/java/com/gotogether/gotogethersbe/repository/OptionRepository.java
@@ -3,5 +3,9 @@ package com.gotogether.gotogethersbe.repository;
 import com.gotogether.gotogethersbe.domain.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OptionRepository extends JpaRepository<Option, Long> {
+
+    List<Option> findByProduct_Id(Long productId);
 }
