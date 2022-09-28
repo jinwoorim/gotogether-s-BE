0046a2fbe480f3fd95fcbba573092a6e3f563b3c
iheese <imbc0224@naver.com>
2022-09-28 20:47:10 +0900

    feat : OptionResponse 추가

--- src/main/java/com/gotogether/gotogethersbe/dto/OptionDto.java
+++ src/main/java/com/gotogether/gotogethersbe/dto/OptionDto.java
@@ -5,18 +5,34 @@ import lombok.Builder;
 
 public class OptionDto {
 
-    @Builder
     public static class OptionRequest{
+        private Long id;
         private String name;
         private String value;
         private Long additional;
 
         public Option toOption(){
             return Option.builder()
+                    .id(id)
                     .name(name)
                     .value(value)
                     .additional(additional)
                     .build();
+        }
+    }
+
+    @Builder
+    public static class OptionResponse{
+        private Long id;
+        private String name;
+        private String value;
+        private Long additional;
+
+        public OptionResponse(Option option){
+            this.id = option.getId();
+            this.name = option.getName();
+            this.value = option.getValue();
+            this.additional = option.getAdditional();
         }
     }
 }
