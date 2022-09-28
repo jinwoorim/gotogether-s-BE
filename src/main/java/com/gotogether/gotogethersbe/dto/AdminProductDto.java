a4249f3359cac92045b213df7203bfd6d0b3057e
iheese <imbc0224@naver.com>
2022-09-28 21:01:06 +0900

    feat : AdminProductDto 필드 수정 및 추가

--- src/main/java/com/gotogether/gotogethersbe/dto/AdminProductDto.java
+++ src/main/java/com/gotogether/gotogethersbe/dto/AdminProductDto.java
@@ -11,6 +11,7 @@ public class AdminProductDto {
 
     @Getter
     public static class ProductRequest{
+        private Long id;
         private String thumbnail;
         private String productName;
         private long amount;
@@ -25,7 +26,7 @@ public class AdminProductDto {
         private String points;
 
         //추천 데이터
-        private Ages ages; //다수 선택으로 변경 예정
+        private String ages;
         private Companion companion;
         private GenderGroup genderGroup;
         private Religion religion;
@@ -33,6 +34,7 @@ public class AdminProductDto {
 
         public Product toProduct(List<Option> optionList){
             return Product.builder()
+                    .id(id)
                     .thumbnail(thumbnail)
                     .productName(productName)
                     .amount(amount)
@@ -49,6 +51,61 @@ public class AdminProductDto {
                     .religion(religion)
                     .theme(theme)
                     .build();
+        }
+    }
+
+    @Getter
+    public static class ProductResponse{
+        private Long id;
+        private String thumbnail;
+        private String productName;
+        private long amount;
+        private String info;
+        private String country;
+        private Continent continent;
+        private List<OptionDto.OptionResponse> optionDtoList;
+
+        //요약 데이터
+        private String region;
+        private String airport;
+        private String points;
+
+        //추천 데이터
+        private String ages;
+        private Companion companion;
+        private GenderGroup genderGroup;
+        private Religion religion;
+        private Theme theme;
+
+        public ProductResponse(Product product, List<OptionDto.OptionResponse> optionDtoList ) {
+            this.id = product.getId();
+            this.thumbnail = product.getThumbnail();
+            this.productName = product.getProductName();
+            this.amount = product.getAmount();
+            this.info = product.getInfo();
+            this.country = product.getCountry();
+            this.continent = product.getContinent();
+            this.optionDtoList = optionDtoList;
+            //요약 데이터
+            this.ages = product.getRegion();
+            this.airport = product.getAirport();
+            this.points = product.getPoints();
+            //추천데이터
+            this.ages = product.getAges();
+            this.companion = product.getCompanion();
+            this.genderGroup = product.getGenderGroup();
+            this.religion = product.getReligion();
+            this.theme = product.getTheme();
+        }
+    }
+
+    public static class ProductListResponse{
+        private Long id;
+        private String productName;
+
+        public ProductListResponse(Product product){
+            this.id = product.getId();
+            this.productName = product.getProductName();
         }
     }
 }
