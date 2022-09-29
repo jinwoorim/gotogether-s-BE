6d31538ecc7e3bc927225ff58728148a3a177032
iheese <imbc0224@naver.com>
2022-09-29 20:31:43 +0900

    feat :  로직 수정 및 에러 수정

--- src/main/java/com/gotogether/gotogethersbe/service/AdminService.java
+++ src/main/java/com/gotogether/gotogethersbe/service/AdminService.java
@@ -1,9 +1,10 @@
 package com.gotogether.gotogethersbe.service;
 
 import com.gotogether.gotogethersbe.config.common.exception.CustomException;
-import com.gotogether.gotogethersbe.domain.Option;
+import com.gotogether.gotogethersbe.domain.Product;
+import com.gotogether.gotogethersbe.domain.ProductOption;
 import com.gotogether.gotogethersbe.dto.AdminProductDto;
-import com.gotogether.gotogethersbe.dto.OptionDto;
+import com.gotogether.gotogethersbe.dto.ProductOptionDto;
 import com.gotogether.gotogethersbe.repository.OptionRepository;
 import com.gotogether.gotogethersbe.repository.ProductRepository;
 import com.gotogether.gotogethersbe.web.api.ResponseMessage;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,29 +26,35 @@ public class AdminService {
     //상품 등록
     @Transactional
     public void insertProduct(AdminProductDto.ProductRequest request){
-        List<Option> optionList  = request.getOptionDtoList()
-                                   .stream()
-                                   .map(optionDto -> optionDto.toOption())
-                                   .toList();
-        optionRepository.saveAll(optionList
-                                   .stream()
-                                   .map(option -> option.addProduct(request.toProduct(optionList)))
-                                   .toList());
-        productRepository.save(request.toProduct(optionList));
+        List<ProductOption> productOptionList = request.getOptionDtoList()
+                                               .stream()
+                                               .map(optionDto -> optionDto.toProductOption())
+                                               .collect(Collectors.toList());
+        Product product = request.toProduct(productOptionList);
+        List<ProductOption> mappedProductOptionList = productOptionList
+                                               .stream()
+                                               .map(option -> option.addProduct(product))
+                                               .toList();
+        productRepository.save(product);
+        optionRepository.saveAll(mappedProductOptionList);
     }
+
+    //아래 로직은 추후 프론트 구현 예정
 
     //상품 수정
     @Transactional
     public void updateProduct(AdminProductDto.ProductRequest request){
-        List<Option> optionList  = request.getOptionDtoList()
-                                    .stream()
-                                    .map(optionDto -> optionDto.toOption())
-                                    .toList();
-        optionRepository.saveAll(optionList
-                                    .stream()
-                                    .map(option -> option.addProduct(request.toProduct(optionList)))
-                                    .toList());
-        productRepository.save(request.toProduct(optionList));
+        List<ProductOption> updatedproductOptionList = request.getOptionDtoList()
+                                                .stream()
+                                                .map(optionDto -> optionDto.toProductOption())
+                                                .collect(Collectors.toList());
+        Product updatedProduct = request.toProduct(updatedproductOptionList);
+        List<ProductOption> mappedProductOptionList = updatedproductOptionList
+                                                .stream()
+                                                .map(option -> option.addProduct(updatedProduct))
+                                                .toList();
+        productRepository.save(updatedProduct);
+        optionRepository.saveAll(mappedProductOptionList);
     }
 
     //상품 삭제
@@ -58,9 +66,9 @@ public class AdminService {
     //상품 상세 조회
     @Transactional(readOnly = true)
     public AdminProductDto.ProductResponse getProduct(Long productId){
-        List<OptionDto.OptionResponse> optionDtoList  = optionRepository.findByProduct_Id(productId)
+        List<ProductOptionDto.OptionResponse> optionDtoList  = optionRepository.findByProduct_Id(productId)
                                                    .stream()
-                                                   .map(OptionDto.OptionResponse::new)
+                                                   .map(ProductOptionDto.OptionResponse::new)
                                                    .toList();
 
         return productRepository.findById(productId)
