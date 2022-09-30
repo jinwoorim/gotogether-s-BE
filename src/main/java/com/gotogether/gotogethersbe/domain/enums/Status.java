b89e40f513fa10df632cd5fb0920bb9a5546b666
jinwoorim <akdlshvlth11@naver.com>
2022-09-30 17:45:01 +0900

    refactor: 생성자 @RequiredArgConstructor로 바꾸고 get메서드 안쓰기 때문에 뺏음

--- src/main/java/com/gotogether/gotogethersbe/domain/enums/Status.java
+++ src/main/java/com/gotogether/gotogethersbe/domain/enums/Status.java
@@ -2,21 +2,16 @@ package com.gotogether.gotogethersbe.domain.enums;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.RequiredArgsConstructor;
 
 @JsonFormat
+@RequiredArgsConstructor
 public enum Status {
 
     STANDBY("예약대기"),
     CANCEL("예약취소"),
     COMPLETE("예약완료");
+
     @JsonValue
     private final String koreanName;
-
-    Status(String koreanName) {
-        this.koreanName = koreanName;
-    }
-
-    public String getKoreanName() {
-        return this.koreanName;
-    }
 }
