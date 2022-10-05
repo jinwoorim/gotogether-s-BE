dd5a039d8a6e19a5743b4a7422348f85bc7c81d0
iheese <imbc0224@naver.com>
2022-10-05 22:51:18 +0900

    conf : redis conf 파일 수정 및 추가

--- src/main/java/com/gotogether/gotogethersbe/config/JwtSecurityConfig.java
+++ src/main/java/com/gotogether/gotogethersbe/config/JwtSecurityConfig.java
@@ -3,6 +3,7 @@ package com.gotogether.gotogethersbe.config;
 import com.gotogether.gotogethersbe.config.auth.TokenManager;
 import com.gotogether.gotogethersbe.config.filter.JwtFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
@@ -11,11 +12,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
     private final TokenManager tokenManager;
+    private final RedisTemplate redisTemplate;
 
     // 직접만든 TokenManager, JwtFilter 를 통해 Security 로직에 필터를 등록
     @Override
     public void configure(HttpSecurity http) {
-        JwtFilter customFilter = new JwtFilter(tokenManager);
+        JwtFilter customFilter = new JwtFilter(tokenManager, redisTemplate);
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
