efce0d207b787712e69dc20fa6a65f1ee3cc138e
khjun723 <67496091+khjun723@users.noreply.github.com>
2022-10-11 06:41:33 +0900

    feat : Add Banner 베너 기능 추가

---
+++ src/main/java/com/gotogether/gotogethersbe/domain/Banner.java
@@ -0,0 +1,19 @@
+package com.gotogether.gotogethersbe.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Banner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "BANNER_ID")
+    private Long id;
+
+    private String BannerImage;
+}
