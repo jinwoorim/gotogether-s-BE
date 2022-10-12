a929e0552c95968182fa78399cfc24acffe448e9
khjun723 <67496091+khjun723@users.noreply.github.com>
2022-10-12 20:35:22 +0900

    feat : 요청 허용 URL /products > /product-list로 변경

--- src/main/java/com/gotogether/gotogethersbe/config/SecurityConfig.java
+++ src/main/java/com/gotogether/gotogethersbe/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
 
             http.csrf().disable() //csrf 토큰 막기
                 .authorizeRequests((authz) -> authz
-                        .antMatchers("/login", "/members", "/reissue", "/logout","/products/*","/members/curation", "/awsrb").permitAll()
+                        .antMatchers("/login", "/members", "/reissue", "/logout","/product-list/*","/members/curation", "/awsrb").permitAll()
                         .antMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
                 ) //위 요청은 모두 허용, 이외 요청은 인증 필수
